1ea0d16eee116b7d7e95603a02dd2e76a9c1378b
Daha Qalbi <qalbichamp5@gmail.com>
2024-06-02 11:02:35 +0500

    updated

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'codingverse.com' title`, () => {
+  it(`should have the 'CodeCraft' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('codingverse.com');
+    expect(app.title).toEqual('CodeCraft');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, codingverse.com');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, CodeCraft');
   });
 });
